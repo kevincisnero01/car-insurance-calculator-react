fcdb982e5e78b5fccfaa60afc80b194d0846349c
Kevin Cisnero <kevingabriel18@gmail.com>
2025-11-01 23:18:40 -0400

    feat(insurance): adding an animation

--- src/components/Quotation/QuoteResult.jsx
+++ src/components/Quotation/QuoteResult.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 //Componente: Resultado de Cotizacion
 const QuoteResult = ({quotation}) => {
+    const nodeRef = useRef(null);
     return ( 
     <>
-        
-        { quotation === 0 ?
+        { quotation === 0 ? (
         <div className='bg-cyan-200 border border-cyan-500 rounded-md p-3 my-2'>
             <p>Elige marca, año y tipo de segunro.</p>
         </div>
-        :
-        <div className='bg-indigo-200 border border-indigo-500 rounded-md p-3 my-2'>
-            <p className='font-medium'>El total es $ <span className='font-bold text-indigo-900'>{quotation}</span></p>
+        ):(
+        <div className='resultado bg-indigo-200 border border-indigo-500 rounded-md p-3 my-2 text-center relative'>
+            <TransitionGroup component={null}>
+                <CSSTransition
+                    nodeRef={nodeRef}
+                    key={quotation}
+                    classNames="resultado"
+                    timeout={{ enter: 500, exit: 500 }}
+                >
+                    <p ref={nodeRef} className='font-medium'>
+                        El total es: <span className='font-bold text-indigo-900'>${quotation}</span>
+                    </p>
+                </CSSTransition>
+            </TransitionGroup>
         </div>
-        }
+        ) }
         
     </>
     );
